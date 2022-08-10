9d5ad7804a15bca2a22916ff3565fd4d5a6d140b
Reza Razavi <reza.razavi@userful.com>
2022-08-10 11:52:05 -0600

    meson build system is set up, #FIXME installation path needs to be corrected as it does not include gstreamer-1.0 on top of libdir

--- compile.sh
+++
@@ -1,5 +0,0 @@
-gcc -Wall -Werror -fPIC $CPPFLAGS $(pkg-config --cflags gstreamer-1.0 $pkg) $(pkg-config --cflags gstreamer-base-1.0 $pkg) $(pkg-config --cflags gstreamer-video-1.0 $pkg) -c -o gstcustomfilter.o gstcustomfilter.c
-
-gcc -shared -o gstcustomfilter.so gstcustomfilter.o $(pkg-config --libs gstreamer-1.0 $pkg) $(pkg-config --libs gstreamer-base-1.0 $pkg) $(pkg-config --libs gstreamer-video-1.0 $pkg)
-
-sudo cp gstcustomfilter.so /usr/lib64/gstreamer-1.0/
